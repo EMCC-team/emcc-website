e3343aa59232f4ec2a2a8224334c2171f00ca099
Tyler Hou <tyler.hou.cs@gmail.com>
2016-08-10 22:01:34 +0000

    changed app.yaml, changed coach model to subclass of more general user model

--- models/Individual.py
+++ models/Individual.py
@@ -1,21 +1,23 @@
 from google.appengine.ext import ndb
-from app.models import Team
+from app.models import Team, User
 
 class Individual(ndb.Model):
     """A model which stores competitors.
     
     Attributes:
+        coach: A key referencing a User.
         name: A string containing the full name of the competitor. (e.g. "Tyler Hou")
-        team: A key value referencing the team which the individual is
-            a competitor of. (e.g. "Houlin Tuna")
         paid: A boolean containing whether the competitor was paid for or not.
             Note that if an individual is part of a paid-for team, then the
             individual is also paid for.
+        team: A key value referencing the team which the individual is
+            a competitor of. (e.g. "Houlin Tuna")
     """
     
-    name = ndb.StringProperty()
-    team = ndb.KeyProperty(kind=Team)
-    _paid = ndb.BooleanProperty()
+    coach = ndb.KeyProperty(kind=User, required=True)
+    name = ndb.StringProperty(required=True)
+    _paid = ndb.BooleanProperty(default=False)
+    team = ndb.KeyProperty(kind=Team, default=None)
     
     @property
     def paid(self):
