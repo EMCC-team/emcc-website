a667a1311a9e8e56adf381a10b214f2f3279d177
Tyler Hou <tyler.hou.cs@gmail.com>
2016-09-07 23:55:20 -0400

    Add email validation and forced lowercase.

--- app/handlers/Authentication.py
+++ app/handlers/Authentication.py
@@ -1,11 +1,12 @@
 import json
+import webapp2
 from BaseHandler import BaseHandler
 from webapp2_extras.auth import InvalidAuthIdError, InvalidPasswordError
 
 class RegisterUserHandler(BaseHandler):
     def post(self):
         name = self.request.get('name')
-        email = self.request.get('email')
+        email = self.request.get('email').lower()
         password = self.request.get('password')
 
         if not name or not email or not password:
@@ -16,6 +17,16 @@ class RegisterUserHandler(BaseHandler):
                 'message': 'Fields name, email, password are required.'
             }))
             return
+
+        if not email.find('@') > 0:
+            self.response.status = '422'
+            self.response.write(json.dumps({
+                'status': '422',
+                'error': 'Unprocessable Entity',
+                'message': 'Invalid email.'
+            }))
+            return
+
 
         auth_id = 'own:' + email
         user = self.user_model.create_user(auth_id, email=email, name=name, password_raw=password)
@@ -29,22 +40,21 @@ class RegisterUserHandler(BaseHandler):
                     'name': name
                 }
             }))
-            print auth_id
             return
 
         self.response.status = '422'
         self.response.write(json.dumps({
             'status': '422',
             'error': 'Unprocessable Entity',
-            'message': 'Users with fields ' + str(user[1]) + ' already exist.',
+            'message': 'A user with that email address already exists.',
             'data': {
-                'fields': user[1]
+                'email': email
             }
         }))
 
 class LoginUserHandler(BaseHandler):
     def post(self):
-        email = self.request.get('email')
+        email = self.request.get('email').lower()
         password = self.request.get('password')
         try:
             u = self.auth.get_user_by_password('own:' + email, password, remember=True)
