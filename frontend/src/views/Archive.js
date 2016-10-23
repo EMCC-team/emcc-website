ab305c3473882e74abcf6dcfcda4e341bef27c46
Tyler Hou <tyler.hou.cs@gmail.com>
2016-10-23 18:11:07 -0400

    Add archive pdf files.

--- frontend/src/views/Archive.js
+++ frontend/src/views/Archive.js
@@ -5,11 +5,38 @@ import '../fonts/Computer-Modern.css';
 
 import { Container, Row, Columns } from '../components/Layout';
 
+let archive = {
+  2016: {
+    all: require('../archive/emcc16all.pdf'),
+    results: require('../archive/emcc16results.pdf')
+  },
+  2015: {
+    all: require('../archive/emcc15all.pdf'),
+    results: require('../archive/emcc15results.pdf')
+  },
+  2014: {
+    all: require('../archive/emcc14all.pdf'),
+  },
+  2013: {
+    all: require('../archive/emcc13all.pdf'),
+  },
+  2012: {
+    all: require('../archive/emcc12all.pdf'),
+  },
+  2011: {
+    all: require('../archive/emcc11all.pdf'),
+  },
+  2010: {
+    all: require('../archive/emcc10all.pdf'),
+  },
+}
+
 class Archive extends React.Component {
   constructor(props){
     super(props);
     this.props.setTitle('Archive');
   }
+
 
   render() {
     return (
@@ -20,21 +47,21 @@ class Archive extends React.Component {
               <h2>EMCC Archive</h2>
             </div>
             <h3>2016</h3>
-            <p><a href="/archive/emcc16all.pdf">EMCC 2016 Problems and Solutions</a></p>
-            <p><a href="/archive/emcc16results.pdf">EMCC 2016 Results</a></p>
+            <p><a href={`${archive['2016'].all}`}>EMCC 2016 Problems and Solutions</a></p>
+            <p><a href={`${archive['2016'].results}`}>EMCC 2016 Results</a></p>
             <h3>2015</h3>
-            <p><a href="/archive/emcc15all.pdf">EMCC 2015 Problems and Solutions</a></p>
-            <p><a href="/archive/emcc15results.pdf">EMCC 2015 Results</a></p>
+            <p><a href={`${archive['2015'].all}`}>EMCC 2015 Problems and Solutions</a></p>
+            <p><a href={`${archive['2015'].results}`}>EMCC 2015 Results</a></p>
             <h3>2014</h3>
-            <p><a href="/archive/emcc14all.pdf">EMCC 2014 Problems and Solutions</a></p>
+            <p><a href={`${archive['2014'].all}`}>EMCC 2014 Problems and Solutions</a></p>
             <h3>2013</h3>
-            <p><a href="/archive/emcc13all.pdf">EMCC 2013 Problems and Solutions</a></p>
+            <p><a href={`${archive['2013'].all}`}>EMCC 2013 Problems and Solutions</a></p>
             <h3>2012</h3>
-            <p><a href="/archive/emcc12all.pdf">EMCC 2012 Problems and Solutions</a></p>
+            <p><a href={`${archive['2012'].all}`}>EMCC 2012 Problems and Solutions</a></p>
             <h3>2011</h3>
-            <p><a href="/archive/emcc11all.pdf">EMCC 2011 Problems and Solutions</a></p>
+            <p><a href={`${archive['2011'].all}`}>EMCC 2011 Problems and Solutions</a></p>
             <h3>2010</h3>
-            <p><a href="/archive/emcc10all.pdf">EMCC 2010 Problems and Solutions</a></p>
+            <p><a href={`${archive['2010'].all}`}>EMCC 2010 Problems and Solutions</a></p>
           </Columns>
         </Row>
       </Container>
