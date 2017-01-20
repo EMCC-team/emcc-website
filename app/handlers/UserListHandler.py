ccb03922ef181f78ea11f531fce58e9004f30d00
Tyler Hou <tyler.hou.cs@gmail.com>
2017-01-19 21:20:26 -0500

    Add API call to get all registered users.

---
+++ app/handlers/UserListHandler.py
@@ -0,0 +1,38 @@
+import json
+import webapp2
+from BaseHandler import BaseHandler
+from app.models.User import User
+from app.models.Team import Team
+
+class UserListHandler(BaseHandler):
+    def get(self):
+        if not self.user:
+            self.response.status = '401'
+            self.response.write(json.dumps({
+                'status': '401',
+                'error': 'Unauthorized',
+            }))
+            return
+        if self.user.email == 'thou@exeter.edu':
+            teams = Team.query(Team.year == 2017, Team.confirmed==True)
+            user_map = {}
+            for team in teams:
+                if user_map.get(team.user.id()):
+                    user_map[team.user.id()] += [team.key.id()]
+                else:
+                    user_map[team.user.id()] = [team.key.id()]
+            users = {team.user for team in teams}
+            users = [user.get() for user in users]
+            users = [{
+                "name": user.name,
+                "email": user.email,
+                "teams": user_map[user.key.id()]
+            } for user in users];
+            self.response.write(json.dumps(users))
+            return
+        self.response.status = '401'
+        self.response.write(json.dumps({
+            'status': '401',
+            'error': 'Unauthorized',
+        }))
+        return
